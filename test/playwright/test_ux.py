cad143cb5c051998e0fff1affe6e76ad7112cf7a
Nikita Karetnikov <nikita@karetnikov.org>
2024-02-17 23:36:41 +0100

    WIP: print all rows

--- test/playwright/test_ux.py
+++ test/playwright/test_ux.py
@@ -193,6 +193,8 @@ def _existing_environment_interactions(page, env_name, time_to_build_env=3*60*10
     # change the description
     page.get_by_placeholder("Enter here the description of your environment").fill("new description")
     # change the vesion spec of an existing package
+    for row in page.get_by_role("row", name="", exact=False).all():
+        print("row:", row.text_content())
     page.get_by_role("row", name="ipykernel", exact=False).get_by_test_id("ConstraintSelectTest").click()
     page.get_by_role("option", name=">=").click()
     # Note: purposefully not testing version constraint since there is inconsistent behavior here
